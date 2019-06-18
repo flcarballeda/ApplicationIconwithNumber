776afcb6f04350434663c93004a3fde569583bb0
Fernando López Carballeda <flc.2056@zohomail.eu>
2019-06-18 11:54:36 +0200

    No funciona, sólo lo añade la primera vez y luego no lo modifica.

--- app/src/main/java/com/example/applicationiconwithnumber/MainActivity.java
+++ app/src/main/java/com/example/applicationiconwithnumber/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.applicationiconwithnumber;
 
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.provider.SyncStateContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -14,6 +17,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private static int count = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,50 +33,78 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 shortcutDel("Application Icon with Number");
                 count++;
-                ((TextView)findViewById(R.id.number)).setText(Integer.toString( count));
+                ((TextView) findViewById(R.id.number)).setText(Integer.toString(count));
                 shortcutAdd("Application Icon with Number", count);
             }
         });
     }
 
     private void shortcutAdd(String name, int number) {
-        // Intent to be send, when shortcut is pressed by user ("launched")
-        Intent shortcutIntent = new Intent(getApplicationContext(), MainActivity.class);
-        // shortcutIntent.setAction(Constants.ACTION_PLAY);
+        PackageManager pm = getApplicationContext().getPackageManager();
+        String packageName = this.getClass().getPackage().getName();
+        // Intent to Start activity
+        ApplicationInfo info = null;
+        try {
+            info = pm.getApplicationInfo(packageName, 0);
 
-        // Create bitmap with number in it -> very default. You probably want to give it a more stylish look
-        Bitmap bitmap = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);
-        Paint paint = new Paint();
-        paint.setColor(0xFF808080); // gray
-        paint.setTextAlign(Paint.Align.CENTER);
-        paint.setTextSize(50);
-        new Canvas(bitmap).drawText(""+number, 50, 50, paint);
+            // Create bitmap with number in it -> very default. You probably want to give it a more stylish look
+            Bitmap bitmap = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);
+            Paint paint = new Paint();
+            paint.setColor(0xFF808080); // gray
+            paint.setTextAlign(Paint.Align.CENTER);
+            paint.setTextSize(50);
+            new Canvas(bitmap).drawText("" + number, 50, 50, paint);
 //        ImageView icn = findViewById(R.mipmap.icon);
 //        icn.setImageBitmap(bitmap);
 
-        // Decorate the shortcut
-        Intent addIntent = new Intent();
-        addIntent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
-        addIntent.putExtra(Intent.EXTRA_SHORTCUT_NAME, name);
-        addIntent.putExtra(Intent.EXTRA_SHORTCUT_ICON, bitmap);
+            if (info != null) {
+                Intent shortcutIntent = pm
+                        .getLaunchIntentForPackage(packageName);
+                if (shortcutIntent != null) {
+                    // Decorate the shortcut
+                    Intent addIntent = new Intent();
+                    addIntent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
+                    addIntent.putExtra(Intent.EXTRA_SHORTCUT_NAME, info.loadLabel(pm));
+                    addIntent.putExtra(Intent.EXTRA_SHORTCUT_ICON, bitmap);
+                    addIntent.putExtra("duplicate", false);
 
-        // Inform launcher to create shortcut
-        addIntent.setAction("com.android.launcher.action.INSTALL_SHORTCUT");
-        getApplicationContext().sendBroadcast(addIntent);
+                    // Inform launcher to create shortcut
+                    addIntent.setAction("com.android.launcher.action.INSTALL_SHORTCUT");
+                    getApplicationContext().sendBroadcast(addIntent);
+                }
+            }
+
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.e( "MIAPP", " Error al añadir el icono del shortcut de la app",  e);
+        }
     }
 
     private void shortcutDel(String name) {
-        // Intent to be send, when shortcut is pressed by user ("launched")
-        Intent shortcutIntent = new Intent(getApplicationContext(), MainActivity.class);
-        // shortcutIntent.setAction(Constants.ACTION_PLAY);
+        PackageManager pm = getApplicationContext().getPackageManager();
+        String packageName = this.getClass().getPackage().getName();
+        // Intent to Start activity
+        ApplicationInfo info = null;
+        try {
+            info = pm.getApplicationInfo(packageName, 0);
 
-        // Decorate the shortcut
-        Intent delIntent = new Intent();
-        delIntent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
-        delIntent.putExtra(Intent.EXTRA_SHORTCUT_NAME, name);
+            if (info != null) {
+                Intent shortcutIntent = pm
+                        .getLaunchIntentForPackage(packageName);
+                if (shortcutIntent != null) {
+                    // Decorate the shortcut
+                    Intent delIntent = new Intent();
+                    delIntent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
+                    delIntent.putExtra(Intent.EXTRA_SHORTCUT_NAME, name);
+                    delIntent.putExtra("duplicate", false);
 
-        // Inform launcher to remove shortcut
-        delIntent.setAction("com.android.launcher.action.UNINSTALL_SHORTCUT");
-        getApplicationContext().sendBroadcast(delIntent);
+                    // Inform launcher to remove shortcut
+                    delIntent.setAction("com.android.launcher.action.UNINSTALL_SHORTCUT");
+                    getApplicationContext().sendBroadcast(delIntent);
+                }
+            }
+
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.e( "MIAPP", " Error al añadir el icono del shortcut de la app",  e);
+        }
     }
 }
