391bd8f2aa92e822a2f48e0b9a966c7b0a529c47
Fernando López Carballeda <flc.2056@zohomail.eu>
2019-06-18 13:28:57 +0200

    No funciona, ya hace la imagen bien.

--- app/src/main/java/com/example/applicationiconwithnumber/MainActivity.java
+++ app/src/main/java/com/example/applicationiconwithnumber/MainActivity.java
@@ -4,8 +4,13 @@ import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.Typeface;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.provider.SyncStateContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -37,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
                 shortcutAdd("Application Icon with Number", count);
             }
         });
+        ((TextView) findViewById(R.id.number)).setText(Integer.toString(count));
     }
 
     private void shortcutAdd(String name, int number) {
@@ -48,14 +54,36 @@ public class MainActivity extends AppCompatActivity {
             info = pm.getApplicationInfo(packageName, 0);
 
             // Create bitmap with number in it -> very default. You probably want to give it a more stylish look
+            Bitmap bitmapIcon = null;// BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+            Drawable dr = getDrawable(R.mipmap.ic_launcher);
+            BitmapDrawable bitmapDrawable = (BitmapDrawable) dr;
+            if(bitmapDrawable.getBitmap() != null) {
+                bitmapIcon = bitmapDrawable.getBitmap();
+            } else {
+                if(dr.getIntrinsicWidth() <= 0 || dr.getIntrinsicHeight() <= 0) {
+                    bitmapIcon = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888); // Single color bitmap will be created of 1x1 pixel
+                } else {
+                    bitmapIcon = Bitmap.createBitmap(dr.getIntrinsicWidth(), dr.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+                }
+
+                Canvas canvas = new Canvas(bitmapIcon);
+                dr.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+                dr.draw(canvas);
+            }
             Bitmap bitmap = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);
             Paint paint = new Paint();
-            paint.setColor(0xFF808080); // gray
             paint.setTextAlign(Paint.Align.CENTER);
+            new Canvas(bitmap).drawBitmap(Bitmap.createScaledBitmap(bitmapIcon, 100, 100, false), 0f, 0f, paint);
+            paint.setColor(0xFF404040); // light gray
             paint.setTextSize(50);
+            paint.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.BOLD));
             new Canvas(bitmap).drawText("" + number, 50, 50, paint);
-//        ImageView icn = findViewById(R.mipmap.icon);
-//        icn.setImageBitmap(bitmap);
+            paint.setColor(0xFF000000); // black
+            paint.setTextSize(50);
+            paint.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.NORMAL));
+            new Canvas(bitmap).drawText("" + number, 50, 50, paint);
+            ImageView icn = findViewById(R.id.icon);
+            icn.setImageBitmap(bitmap);
 
             if (info != null) {
                 Intent shortcutIntent = pm
